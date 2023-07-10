899190743d9023745afe7a1d88cabdbaaa62ece7
Sallam Rady <sallamrady@gmail.com>
2023-07-11 00:35:28 +0300

    bit manipulations

---
+++ bit-manipulation/bit-maipulation.js
@@ -0,0 +1,62 @@
+function decimalToBinary(num, res = "") {
+  if (num <= 1) {
+    console.log(num);
+    return res;
+  }
+  decimalToBinary(parseInt(num / 2), res);
+  console.log(num % 2);
+}
+
+function isOdd(num) {
+  return num & 1;
+}
+
+function countBits1(num) {
+  let counter = 0;
+  while (num) {
+    counter++;
+    num = num & (num - 1); //remove first 1 from right
+  }
+  return counter;
+}
+
+//index from right / 0-based
+function getBit(num, idx) {
+  num = num >> idx;
+  return num & 1;
+}
+
+function setBit1(num, idx) {
+  return num | (1 << idx);
+}
+
+function setBit0(num, idx) {
+  return num & ~(1 << idx);
+}
+
+function flibBit(num, idx) {
+  return num ^ (1 << idx);
+}
+
+function isPowerOf2(num) {
+  let cnt = countBits1(num);
+  return cnt == 1;
+}
+
+function numOfBits(num) {
+  let cnt = 1;
+  while (1 << cnt < num) {
+    cnt++;
+  }
+  return cnt;
+}
+
+function makeFirst1to0(num) {
+  return num & (num - 1);
+}
+
+function lastBit1Value(num) {
+  return num & ~(num - 1);
+}
+
+console.log(lastBit1Value(22));
---
+++ bit-manipulation/bitMask-example.cpp
@@ -0,0 +1,96 @@
+/// import language labs.
+#include <iostream>
+#include <map>
+#include <string>
+#include <cmath>
+#include <math.h>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <bitset>
+#include <set>
+#include <stack>
+#include <iomanip>
+#include <queue>
+///---------------Helpers.-------------///
+#define endl "\n"
+#define ll long long
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);
+#define all(a) a.begin(), a.end()
+#define scan(v)        \
+    for (auto &it : v) \
+        cin >> it;
+#define mod(a, n) (a % n + n) % n
+///----------use namespace std.----------///
+using namespace std;
+///--------File Function to get input from file.-
+void File()
+{
+#ifndef ONLINE_JUDGE
+    // freopen("Input.txt", "r", stdin);
+    // freopen("Output.txt", "w", stdout);
+#endif
+}
+
+///***** Divide and conquer *****///
+
+int getBit(int num, int idx)
+{
+    num = num >> idx;
+    return num & 1;
+}
+
+int Solution();
+int main()
+{
+    //** Prepare my Code **//
+    fast;
+    //** Solution **//
+    int result = Solution();
+    cout << result << endl;
+}
+int Solution()
+{
+    /// Declaration ...
+    int n, m, k, mx = 0;
+
+    /// Take input from user...
+    cin >> n >> m;
+    vector<pair<int, int>> conditions(m);
+    for (auto &a : conditions)
+        cin >> a.first >> a.second;
+    cin >> k;
+    vector<pair<int, int>> balls(k);
+    for (auto &a : balls)
+        cin >> a.first >> a.second;
+    /// Processing...
+    for (int i = 0; i < (1 << k); i++)
+    {
+        vector<int> frq(n + 1);
+        for (int j = 0; j < k; j++)
+        {
+            if (getBit(i, j) == 0)
+            {
+                frq[balls[j].first]++;
+            }
+            else
+            {
+                frq[balls[j].second]++;
+            }
+        }
+        int cnt = 0;
+        for (int j = 0; j < m; j++)
+        {
+            int a = conditions[j].first, b = conditions[j].second;
+            if (frq[a] >= 1 && frq[b] >= 1)
+                cnt++;
+        }
+        mx = max(mx, cnt);
+    }
+    /// Output...
+    cout << "mx :" << mx << endl;
+    return mx;
+}
