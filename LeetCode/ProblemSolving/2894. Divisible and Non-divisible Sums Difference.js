8861fc759cc7f0b71ab6782e3b2869892a3acd02
Sallam Rady <sallamrady@gmail.com>
2024-04-30 23:14:11 +0300

    leet code today problem

---
+++ LeetCode/ProblemSolving/2894. Divisible and Non-divisible Sums Difference.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var differenceOfSums = function (n, m) {
+  let num1 = 0,
+    num2 = 0;
+  for (let i = 1; i <= n; i++) {
+    if (i % m != 0) num1 += i;
+    else num2 += i;
+  }
+  return num1 - num2;
+};
