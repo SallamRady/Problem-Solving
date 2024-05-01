095b35f68a307c4efda6662a0f610d49439adbd7
Sallam Rady <sallamrady@gmail.com>
2024-05-01 19:58:17 +0300

    remindering problems in sheet 2

---
+++ codeforces/Assuit-ICPC/03.Sheet-2/R.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <bitset>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <climits>
+#include <stack>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <unordered_set>
+#define endl "\n"
+#define ll long long
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);
+#define all(a) a.begin(), a.end()
+using namespace std;
+
+// * Check given number is prime or not ??
+bool IsPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+// * Get number divisors
+vector<int> NumberDivisors(ll n)
+{
+    // * Declare and define variables....
+    vector<int> divisors;
+    // * Proccessing....
+    for (int i = 1; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            divisors.push_back(i);
+
+            if (n / i != i)
+                divisors.push_back(n / i);
+        }
+    }
+    sort(divisors.begin(), divisors.end());
+    return divisors;
+}
+
+// * Calc Greatest Common Divisor (GCD) of two numbers
+ll GCD(ll n, ll m)
+{
+    return __gcd(n, m);
+}
+
+// * Check if Given number is lucky
+bool IsLuckyNumber(ll n)
+{
+    // Lucky number is any positive number that its decimal representation contains only 4 and 7.
+    int digit = 0;
+
+    while (n)
+    {
+        digit = n % 10;
+        if (digit != 4 && digit != 7)
+            return false;
+        n /= 10;
+    }
+    return true;
+}
+// TODO::declare and define helper functions and variables
+void solution()
+{
+    // *declare our variables..
+    int n, m, sum = 0;
+    // *input
+    // *proccessing...
+    while (cin >> n >> m)
+    {
+        if (n <= 0 || m <= 0)
+            return;
+        sum = 0;
+        if (n > m)
+            swap(n, m);
+        for (; n <= m; n++)
+        {
+            sum += n;
+            cout << n << " ";
+        }
+        cout << "sum =" << sum << endl;
+    }
+    // *Output....
+};
+
+int main()
+{
+    //* prepare..code
+    fast;
+    // declaration
+    solution();
+
+    return 0;
+}
---
+++ codeforces/Assuit-ICPC/03.Sheet-2/X.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <bitset>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <climits>
+#include <stack>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <unordered_set>
+#define endl "\n"
+#define ll long long
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);
+#define all(a) a.begin(), a.end()
+using namespace std;
+
+// * Check given number is prime or not ??
+bool IsPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+// * Get number divisors
+vector<int> NumberDivisors(ll n)
+{
+    // * Declare and define variables....
+    vector<int> divisors;
+    // * Proccessing....
+    for (int i = 1; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            divisors.push_back(i);
+
+            if (n / i != i)
+                divisors.push_back(n / i);
+        }
+    }
+    sort(divisors.begin(), divisors.end());
+    return divisors;
+}
+
+// * Calc Greatest Common Divisor (GCD) of two numbers
+ll GCD(ll n, ll m)
+{
+    return __gcd(n, m);
+}
+
+// * Check if Given number is lucky
+bool IsLuckyNumber(ll n)
+{
+    // Lucky number is any positive number that its decimal representation contains only 4 and 7.
+    int digit = 0;
+
+    while (n)
+    {
+        digit = n % 10;
+        if (digit != 4 && digit != 7)
+            return false;
+        n /= 10;
+    }
+    return true;
+}
+
+// * Get Summation of digits of number
+int SumOfDigits(int n)
+{
+    int sum = 0;
+    while (n)
+    {
+        sum += (n % 10);
+        n /= 10;
+    }
+    return sum;
+};
+
+// * Convert number(decimal) to string
+string toBinary(ll n)
+{
+    string result;
+    while (n != 0)
+    {
+        result = (n % 2 == 0 ? "0" : "1") + result;
+        n /= 2;
+    }
+
+    return result;
+}
+
+// TODO::declare and define helper functions and variables
+void solution()
+{
+    // *declare our variables..
+    int testcases, n, onesCount = 0;
+    ll result;
+    string binary;
+    // *input
+    cin >> testcases;
+    // *proccessing...
+    while (testcases--)
+    {
+        cin >> n;
+        binary = toBinary(n);
+        onesCount = 0;
+        result = 0;
+
+        for (auto b : binary)
+            if (b == '1')
+                onesCount++;
+
+        for (int i = 0; i < onesCount; i++)
+        {
+            result += (ll)pow(2, i);
+        }
+
+        cout << result << endl;
+    }
+
+    // *Output....
+};
+
+int main()
+{
+    //* prepare..code
+    fast;
+    // declaration
+    solution();
+
+    return 0;
+}
