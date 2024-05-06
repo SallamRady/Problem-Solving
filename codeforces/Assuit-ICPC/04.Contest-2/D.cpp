7ca63b1913ac325867b2a4479c15b2a21312bf18
Sallam Rady <sallamrady@gmail.com>
2024-05-06 10:19:49 +0300

    contest num 2

---
+++ codeforces/Assuit-ICPC/04.Contest-2/D.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <bitset>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <climits>
+#include <stack>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <unordered_set>
+#define endl "\n"
+#define ll long long
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);
+#define all(a) a.begin(), a.end()
+using namespace std;
+
+// * Check given number is prime or not ??
+bool IsPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+// * Get number divisors
+vector<int> NumberDivisors(ll n)
+{
+    // * Declare and define variables....
+    vector<int> divisors;
+    // * Proccessing....
+    for (int i = 1; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            divisors.push_back(i);
+
+            if (n / i != i)
+                divisors.push_back(n / i);
+        }
+    }
+    sort(divisors.begin(), divisors.end());
+    return divisors;
+}
+
+// * Calc Greatest Common Divisor (GCD) of two numbers
+ll GCD(ll n, ll m)
+{
+    return __gcd(n, m);
+}
+
+// * Check if Given number is lucky
+bool IsLuckyNumber(ll n)
+{
+    // Lucky number is any positive number that its decimal representation contains only 4 and 7.
+    int digit = 0;
+
+    while (n)
+    {
+        digit = n % 10;
+        if (digit != 4 && digit != 7)
+            return false;
+        n /= 10;
+    }
+    return true;
+}
+
+// * Get Summation of digits of number
+int SumOfDigits(int n)
+{
+    int sum = 0;
+    while (n)
+    {
+        sum += (n % 10);
+        n /= 10;
+    }
+    return sum;
+};
+
+// * Convert number(decimal) to string
+string toBinary(ll n)
+{
+    string result;
+    while (n != 0)
+    {
+        result = (n % 2 == 0 ? "0" : "1") + result;
+        n /= 2;
+    }
+
+    return result;
+}
+
+// * Get first n elements in Fibonacci
+void Fibonacci(int n)
+{
+    ll base0 = 0, base1 = 1, temp;
+    for (int i = 0; i < n; i++)
+    {
+        switch (i)
+        {
+        case 0:
+            cout << base0 << " ";
+            break;
+        case 1:
+            cout << base1 << " ";
+            break;
+
+        default:
+            temp = base0 + base1;
+            cout << temp << " ";
+            base0 = base1;
+            base1 = temp;
+            break;
+        }
+    }
+}
+
+// TODO::declare and define helper functions and variables
+void solution()
+{
+    // *declare our variables..
+    int testcases;
+    ll n, m, sum1 = 0, sum2 = 0;
+    // *input
+    cin >> testcases;
+    // *proccessing...
+    while (testcases--)
+    {
+        cin >> n >> m;
+        // m is a greater.
+        if (n > m)
+            swap(n, m);
+
+        sum1 = (n * (n + 1)) / 2;
+        sum2 = (m * (m + 1)) / 2;
+
+        cout << (sum2 - sum1 + n) << endl;
+    }
+
+    // *Output....
+};
+
+int main()
+{
+    //* prepare...code...
+    fast;
+    // declaration
+    solution();
+
+    return 0;
+}
---
+++ codeforces/Assuit-ICPC/04.Contest-2/F.cpp
@@ -0,0 +1,160 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <bitset>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <climits>
+#include <stack>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <unordered_set>
+#define endl "\n"
+#define ll long long
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);
+#define all(a) a.begin(), a.end()
+using namespace std;
+
+// * Check given number is prime or not ??
+bool IsPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+// * Get number divisors
+vector<int> NumberDivisors(ll n)
+{
+    // * Declare and define variables....
+    vector<int> divisors;
+    // * Proccessing....
+    for (int i = 1; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            divisors.push_back(i);
+
+            if (n / i != i)
+                divisors.push_back(n / i);
+        }
+    }
+    sort(divisors.begin(), divisors.end());
+    return divisors;
+}
+
+// * Calc Greatest Common Divisor (GCD) of two numbers
+ll GCD(ll n, ll m)
+{
+    return __gcd(n, m);
+}
+
+// * Check if Given number is lucky
+bool IsLuckyNumber(ll n)
+{
+    // Lucky number is any positive number that its decimal representation contains only 4 and 7.
+    int digit = 0;
+
+    while (n)
+    {
+        digit = n % 10;
+        if (digit != 4 && digit != 7)
+            return false;
+        n /= 10;
+    }
+    return true;
+}
+
+// * Get Summation of digits of number
+int SumOfDigits(int n)
+{
+    int sum = 0;
+    while (n)
+    {
+        sum += (n % 10);
+        n /= 10;
+    }
+    return sum;
+};
+
+// * Convert number(decimal) to string
+string toBinary(ll n)
+{
+    string result;
+    while (n != 0)
+    {
+        result = (n % 2 == 0 ? "0" : "1") + result;
+        n /= 2;
+    }
+
+    return result;
+}
+
+// * Get first n elements in Fibonacci
+void Fibonacci(int n)
+{
+    ll base0 = 0, base1 = 1, temp;
+    for (int i = 0; i < n; i++)
+    {
+        switch (i)
+        {
+        case 0:
+            cout << base0 << " ";
+            break;
+        case 1:
+            cout << base1 << " ";
+            break;
+
+        default:
+            temp = base0 + base1;
+            cout << temp << " ";
+            base0 = base1;
+            base1 = temp;
+            break;
+        }
+    }
+}
+
+// TODO::declare and define helper functions and variables
+void solution()
+{
+    // *declare our variables..
+    ll n, x;
+    int count = 0, mx = 0;
+    // *input
+    cin >> n;
+    // *proccessing...
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        count = 0;
+        while (x % 2 == 0 && x > 0)
+        {
+            count++;
+            x /= 2;
+        }
+        mx = max(mx, count);
+    }
+    // *Output....
+    cout << mx << endl;
+};
+
+int main()
+{
+    //* prepare...code...
+    fast;
+    // declaration
+    solution();
+
+    return 0;
+}
