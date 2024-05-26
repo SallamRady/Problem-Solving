798db016b330ab164bc3e11be8c2c04c38bbf782
Sallam Rady <sallamrady@gmail.com>
2024-05-26 10:58:45 +0300

    problem on string in leet code

---
+++ LeetCode/ProblemSolving/3110. Score of a String.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var scoreOfString = function (s) {
+  let n = s.length,
+    sum = 0;
+  for (let i = 0; i < n - 1; i++) {
+    sum += Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1));
+  }
+  return sum;
+};
